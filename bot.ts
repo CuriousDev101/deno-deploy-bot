739c455193e90f2a2265ecefb47a730f891f845d
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-08 19:04:56 +0300

    test webhook

--- bot.ts
+++ bot.ts
@@ -1,7 +1,15 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Bot } from "./deps.deno.ts";
+import { webhookCallback } from "./deps.deno.ts";
 import "jsr:@std/dotenv/load";
 
+const app = new Application(); // or whatever you're using
+
+// Make sure to specify the framework you use.
+
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+
+app.use(webhookCallback(bot, "oak"));
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
