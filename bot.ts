597046315270fa814a4e6bf9947360c71b551c20
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-08 18:39:49 +0300

    first commit

---
+++ bot.ts
@@ -0,0 +1,10 @@
+import { Bot } from "./deps.deno.ts";
+import "jsr:@std/dotenv/load";
+
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+bot.start()
