1d367257b845dca260efaf7027f9c6ab007c8450
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-08 18:57:51 +0300

    rm bot start

--- bot.ts
+++ bot.ts
@@ -6,5 +6,3 @@ export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
-
-bot.start()
